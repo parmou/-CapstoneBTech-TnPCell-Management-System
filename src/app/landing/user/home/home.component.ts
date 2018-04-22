0a48fe1a21900cabde4949561f13da8785ba3a82
pm17 <parveshmourya96@gmail.com>
2018-04-22 05:43:10 +0530

    home change

--- src/app/landing/user/home/home.component.ts
+++ src/app/landing/user/home/home.component.ts
@@ -8,15 +8,26 @@ import {LOCAL_STORAGE, WebStorageService} from 'angular-webstorage-service';
 })
 export class HomeComponent implements OnInit {
   username: String;
-  email: String;
+   isCoordinator: String;
+  rollno: String;
+  year: String;
+  branch: String;
+ email: String;
   constructor(@Inject(LOCAL_STORAGE) private storage: WebStorageService) {
     this.username = this.storage.get('name');
     this.email = this.storage.get('isCoordinator');
+    this.rollno = this.storage.get('rollno');
+    this.year = this.storage.get('year');
+    this.branch = this.storage.get('branch'); 
    }
 
   ngOnInit() {
    this.username = this.storage.get('name');
     this.email = this.storage.get('isCoordinator');
+    this.rollno = this.storage.get('rollno');
+    this.year = this.storage.get('year');
+    this.branch = this.storage.get('branch');
+
     
   }
 
